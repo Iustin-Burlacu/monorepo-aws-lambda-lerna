f52e16736f5af9b053fc1821d342ab630ffa128a
Iustin-Burlacu <50848825+Iustin-Burlacu@users.noreply.github.com>
2024-03-18 17:56:00 +0100

    fixed compile scripts in root package.json and added other lib test

---
+++ Lib/A/src/index.ts
@@ -0,0 +1,12 @@
+import { Log } from "@my/log";
+export class A {
+    private log;
+
+    public constructor(log: Log) {
+        this.log = log;
+    }
+
+    async test() {
+        this.log.debug('test')
+    }
+}
